8a0029dfe28ef5c591e2690d923e5ed250f744fe
cmdras <cmdras94@gmail.com>
2016-11-10 17:48:30 +0100

    Update Visualisatie-RushHour.py

--- Visualisatie/Visualisatie-RushHour.py
+++ Visualisatie/Visualisatie-RushHour.py
@@ -1,7 +1,7 @@
 import Tkinter
 from vehicle import vehicle
 
-CAR_ID = {'A': 'green', 'B': 'CadetBlue', 'C': 'RoyalBlue1',
+CAR_ID = {'A': 'cyan2', 'B': 'CadetBlue', 'C': 'RoyalBlue1',
           'D': 'dark orange', 'T': 'red2', ' ': 'grey'}
 
 TRUCKS_ID = {'E': 'MediumPurple1', 'F': 'yellow2', 'Fa': 'yellow2',
@@ -25,23 +25,24 @@ class Board(Tkinter.Frame):
             vehicles.append(vehicle(vehicle_id, int(vehicle_row_v), int(vehicle_col_v), vehicle_or, self.width))
         table = table_retriever(vehicles, self.width)
         # format of informationboard
+        # move_vehicles(table, vehicles)
         visualize(table, self.width, self.height)
         # prints the informationboard
 
 
 def table_retriever(vehicles, size):
-
-
+    
+    
     table = [[' ' for i in xrange(size)] for i in xrange(size)]
 
     for vehicle in vehicles:
         row_v, col_v = vehicle.row_v, vehicle.col_v
         if vehicle.orientation == 'hor':
             for i in range(vehicle.length):
-                table[row_v + 1][col_v] = vehicle.id
+                table[row_v][col_v+i] = vehicle.id
         else:
             for i in range(vehicle.length):
-                table[row_v][col_v + 1] = vehicle.id
+                table[row_v+i][col_v] = vehicle.id
     return table
 
 
@@ -66,12 +67,39 @@ def start_rushhour():
         game = str(raw_input("Which board do you want to play?: "))
         if game[0] == "a" or game[0] == "b" or game[0] == "c":
             if game == "a1" or game == "a2" or game == "a3":
-                i = a
+                board_size = a
             if game == "b1" or game == "b2" or game == "b3":
-                i = b
+                board_size = b
             if game == "c":
-                i = c
+                board_size = c
             break
-    Board(i, game)
+    Board(board_size, game)
+
+# TODO
+"""
+def move_vehicles(table, vehicles):
+    move_vehicle = []
+
+    for vehicle in vehicles:
+        if vehicle.orientation == 'hor' and vehicle.length < 3:
+            if (table[vehicle.row_v][vehicle.col_v - 1]  == ' ' && vehicle.row_v > 0 && vehicle_row_v <= 5) or (table[vehicle.row_v][vehicle.col_v + 2] == ' ' && vehicle.row_v > 0 && vehicle_row_v <= 5):
+                print '1'
+                move_vehicle.append(vehicle)
+        elif vehicle.orientation == 'hor':
+            if (table[vehicle.row_v][vehicle.col_v -1] == ' ' && vehicle.row_v > 0 && vehicle_row_v <= 5) or (table[vehicle.row_v][vehicle.col_v + 3] == ' ' && vehicle.row_v > 0 && vehicle_row_v <= 5):
+                print '2'
+                move_vehicle.append(vehicle)
+        elif vehicle.orientation == 'ver' and vehicle.length < 3:
+            if (table[vehicle.row_v][vehicle.col_v - 1] == ' ' or table[vehicle.row_v + 2][vehicle.col_v] == ' ':
+                print '3'
+                move_vehicle.append(vehicle)
+        elif vehicle.orientation == 'ver':
+            if table[vehicle.row_v][vehicle.col_v - 1] == ' ' or table[vehicle.row_v][vehicle.col_v + 3] == ' ':
+                print '4'
+                move_vehicle.append(vehicle)
+
+    print(move_vehicle)
+
+"""
 
 start_rushhour()
