3a7fd327fd691e13a3eb11d7ac9a77893a1af577
cmdras <cmdras94@gmail.com>
2016-12-22 20:30:03 +0100

    Update astar.py

--- Visualisatie/nieuw/astarV2/astar.py
+++ Visualisatie/nieuw/astarV2/astar.py
@@ -1,86 +1,108 @@
+import Tkinter
+import itertools
+import copy
 import sys
 import time
+
 from vehicle import vehicle
 from collections import deque
 from helpers import *
+from solvers import *
 
-# class om de parents en score bij te houden van een bordconfiguratie
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.parent = None
-        self.score = 0
+class Board(object):
+    def __init__(self, width, board_level):
+        self.width = width
+        self.height = width
+        board_level = "vehicles/" + board_level + ".txt"
+        self.vehicles = start_vehicles(board_level, width)
+        self.all_positions = {}
+        self.x = ''
 
-# de A* solver
-def astar_solver_all(table, width, s_kind):
-    all_positions = table.all_positions
-    visited = 0
-    start_time = datetime.datetime.now()
-    print "**********"
-    openset = set()
-    closedset = []
+    def table_retriever(self):
+        table = [[' ' for i in xrange(self.width)] for i in xrange(self.width)]
+        for vehicle in self.vehicles:
+            row_v, col_v = vehicle.row_v, vehicle.col_v
+            if vehicle.orientation == 'hor':
+                for i in range(vehicle.length):
+                    table[row_v][col_v + i] = vehicle.id
+            elif vehicle.orientation == 'ver':
+                for i in range(vehicle.length):
+                    table[row_v + i][col_v] = vehicle.id
+        if game_win(table):
+            print "You Win!"
+            sys.exit()
+        return table
 
-    current = Node(table.vehicles)
-    openset.add(current)
-    closedset.append(table_retriever(width, current.value))
-    while openset:
-        if s_kind == "block" or s_kind == "taxi":
-            current = min(openset, key=lambda o: o.score)
-        if s_kind == "free" or s_kind == "children":
-            current = max(openset, key=lambda o: o.score)
+    def move_vehicle(self):
+        self.x = table_retriever(self.width, self.vehicles)
+        table_queue = self.x
+        for car in self.vehicles:
+            if car.orientation == 'hor':
+                if car.col_v - 1 >= 0 and table_queue[car.row_v][car.col_v - 1] == ' ':
+                    for new_car in self.all_positions[car.id]:
+                        if car.col_v - 1 == new_car.col_v:
+                            children = add_new_car(self.vehicles, car, new_car)
+                            yield children
 
-        current = min(openset, key=lambda o: o.score)
-        x = deque()
-        x.append(table_retriever(width, current.value))     
+                if car.col_v + car.length <= self.width - 1 and table_queue[car.row_v][car.col_v + car.length] == ' ':
+                    for new_car in self.all_positions[car.id]:
+                        if car.col_v + 1 == new_car.col_v:
+                            children = add_new_car(self.vehicles, car, new_car)
+                            yield children
 
-        if game_win(current.value, width):
-            print "You win!"
-            end_time = datetime.datetime.now()
-            elapsed = end_time - start_time
-            print "Time elapsed: " + str(elapsed.seconds) + " seconds and " + str(elapsed.microseconds) + " microseconds."
-            winning_moves = node_traversal(current)
-            filename = s_kind + '_a1.txt'
-            f = open(filename, 'w')
-            for node in reversed(winning_moves):
-                j = table_retriever(width, node.value)
-                for x in j:
-                    f.write(repr(x))
-                    f.write("\n")
-                f.write("--------------------\n")
-            f.write(str(visited))
-            f.write("\n")
-            f.write("Time elapsed: " + str(elapsed.seconds) + " seconds and " + str(elapsed.microseconds) + " microseconds.")
-            f.write("\n")
-            f.write("number of moves: " + str(len(winning_moves)))
+            if car.orientation == 'ver':
+                if car.row_v - 1 >= 0 and table_queue[car.row_v - 1][car.col_v] == ' ':
+                    for new_car in self.all_positions[car.id]:
+                        if car.row_v - 1 == new_car.row_v:
+                            children = add_new_car(self.vehicles, car, new_car)
+                            yield children
+
+                if car.row_v + car.length <= self.width - 1 and table_queue[car.row_v + car.length][car.col_v] == ' ':
+                    for new_car in self.all_positions[car.id]:
+                        if car.row_v + 1 == new_car.row_v:
+                            children = add_new_car(self.vehicles, car, new_car)
+                            yield children
 
 
-            f.close()
-            print ">>> A Star"
-            print "Data Size: %d" % (visited)
-            print "number of moves: " + str(len(winning_moves))
+
+def start_vehicles(board_level, width):
+    vehicles = []
+    text_file = open(board_level, "r")
+    lines = text_file.read().split('\n')
+    for i in lines:
+        j = i.split(',')
+        vehicle_id = j[0]
+        vehicle_row_v = j[1]
+        vehicle_col_v = j[2]
+        vehicle_or = j[3]
+        vehicles.append(vehicle(vehicle_id, int(vehicle_row_v), int(vehicle_col_v), vehicle_or, width))
+    return vehicles
+
+
+def start_rushhour():
+    # type: () -> object
+    a = 6
+    b = 9
+    c = 12
+    print "Difficulty  1 = easy, 2 = normal, 3 = hard"
+    print "A is 6*6, B is 9*9, C is 12*12"
+    print "Choice option example: option_a1"
+    while True:
+        game = str(raw_input("Which board do you want to play?: "))
+        if game[0] == "a" or game[0] == "b" or game[0] == "c":
+            if game == "a1" or game == "a2" or game == "a3":
+                board_size = a
+            if game == "b1" or game == "b2" or game == "b3":
+                board_size = b
+            if game == "c":
+                board_size = c
             break
-        
-        openset.remove(current)
-        if x not in closedset:
-            closedset.append(x)
-        table.vehicles = current.value
-        children = table.move_vehicle()
+    print "You can pick solver: block, free, taxi or children"
+    solver = str(raw_input("which solver?: "))
 
-        for child in children:
-            y = table_retriever(width, child)
-            if y in closedset:
-                continue
-            node = Node(child)
-            if s_kind == "block":
-                node.score = find_blocking_cars(node.value, width)
-            if s_kind == "free":
-                node.score = find_free_path(node.value, width)
-            if s_kind == "taxi":
-                node.score = taxi_priority(node.value, width)
-            if s_kind == "children":
-                node.score = children_score(child, width, all_positions)
-            node.parent = current
-            openset.add(node)
-            closedset.append(y)
-            visited += 1
-            print visited
+    board = Board(board_size, game)
+    board.all_positions = all_possible_vehicles(board.vehicles, board_size)
+
+    astar_solver_all(board, board_size, solver)
+
+start_rushhour()
