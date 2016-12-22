36dfcd99034e524c8ae36b848ebc23d7dcd135c2
JasperLe <lelijveldj@hotmail.com>
2016-12-22 15:39:32 +0100

    Update Breadth-first.py

--- Visualisatie/nieuw/Breadth-First/Breadth-first.py
+++ Visualisatie/nieuw/Breadth-First/Breadth-first.py
@@ -14,13 +14,14 @@ TRUCKS_ID = {'E': 'MediumPurple1', 'F': 'yellow2', 'Fa': 'yellow2',
              'G': 'gold', 'H': 'orange', 'I': 'gray64'}
 
 
-# creeer een class genaamd Queue met de volgende methods:__init__, insert en remove(pop)
+# creeer een class genaamd node, met eigenschappen: value en parant
 class Node:
     def __init__(self, value):
         self.value = value
         self.parent = None
 
 
+# class Board als infrastructuur voor solvers
 class Board(object):
     def __init__(self, width, board_level):
         self.width = width
@@ -30,6 +31,7 @@ class Board(object):
         self.all_positions = {}
         self.x = ''
 
+    # functie om de 2d array te bouwen op basis van de vehicle array
     def table_retriever(self):
         table = [[' ' for i in xrange(self.width)] for i in xrange(self.width)]
         for vehicle in self.vehicles:
@@ -45,57 +47,60 @@ class Board(object):
             sys.exit()
         return table
 
+    # functie om de children te yielden
     def move_vehicle(self):
         self.x = table_retriever(self.width, self.vehicles)
         table_queue = self.x
         for car in self.vehicles:
             if car.orientation == 'hor':
+                # links check
                 if car.col_v - 1 >= 0 and table_queue[car.row_v][car.col_v - 1] == ' ':
                     for new_car in self.all_positions[car.id]:
                         if car.col_v - 1 == new_car.col_v:
-                            # vanaf hieronder kan in eigen functie
                             children = copy.deepcopy(self.vehicles)
                             children.remove(car)
                             children.append(new_car)
                             yield children
-
+                # rechts check
                 if car.col_v + car.length <= (self.width-1) and table_queue[car.row_v][car.col_v + car.length] == ' ':
                     for new_car in self.all_positions[car.id]:
                         if car.col_v + 1 == new_car.col_v:
-                            # vanaf hieronder kan in eigen functie
                             children = copy.copy(self.vehicles)
                             children.remove(car)
                             children.append(new_car)
                             yield children
 
             if car.orientation == 'ver':
+                # boven check
                 if car.row_v - 1 >= 0 and table_queue[car.row_v - 1][car.col_v] == ' ':
                     for new_car in self.all_positions[car.id]:
                         if car.row_v - 1 == new_car.row_v:
-                            # vanaf hieronder kan in eigen functie
                             children = copy.copy(self.vehicles)
                             children.remove(car)
                             children.append(new_car)
                             yield children
 
+                # onder check
                 if car.row_v + car.length <= (self.width-1) and table_queue[car.row_v + car.length][car.col_v] == ' ':
                     for new_car in self.all_positions[car.id]:
                         if car.row_v + 1 == new_car.row_v:
-                            # vanaf hieronder kan in eigen functie
                             children = copy.copy(self.vehicles)
                             children.remove(car)
                             children.append(new_car)
                             yield children
 
-
-def astar_solver(table, width):
+# solver
+def breadth_first_solver(table, width):
     i = 0
     start_time = datetime.datetime.now()
     print "**********"
+    # deque = rij
     openset = deque()
+    # set = archive
     closedset = set()
     current = Node(table.vehicles)
     openset.append(current)
+    # main solver loop
     while openset:
         current = openset.pop()
         if game_win2(current.value):
@@ -105,6 +110,7 @@ def astar_solver(table, width):
             print "Time elapsed: " + str(elapsed.seconds) + " seconds and " + str(elapsed.microseconds) + " microseconds."
             winning_moves = node_traversal(current)
             f = open('A33bf.txt', 'w')
+            # schrijf naar .txt voor archivering resultaten
             for node in reversed(winning_moves):
                 j = table_retriever(width, node.value)
                 for x in j:
@@ -117,10 +123,9 @@ def astar_solver(table, width):
             print "number of moves: " + str(len(winning_moves))
             break
 
-        # maak deque hiervan
-
         table.vehicles = current.value
         children = table.move_vehicle()
+        # Voor ieder kind in children check of ze in archive zitten en voeg evt. toe aan archive
         for child in children:
             y = table_retriever(width, child)
             y = repr(y)
@@ -134,13 +139,14 @@ def astar_solver(table, width):
             print i
 
 
+# aanpassen voor a,b en c borden (a=4,b=7,c=10)
 def game_win2(vehicle_array):
     for vehicle in vehicle_array:
         if vehicle.id == "T" and vehicle.col_v == 7:
             return True
     return False
 
-
+# laden van het startpunt
 def start_vehicles(board_level, width):
     vehicles = []
     text_file = open(board_level, "r")
@@ -175,10 +181,7 @@ def start_rushhour():
             break
     board = Board(board_size, game)
     board.all_positions = all_possible_vehicles(board.vehicles, board_size)
-    astar_solver(board, board_size)
-
-
-    # print board.x
+    breadth_first_solver(board, board_size)
 
 
 start_rushhour()
